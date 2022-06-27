26cb173c16531c227fe790d227e7e20236c8143e
Sven Ponelat <svenponelat@hotmail.com>
2022-06-27 09:41:45 -0300

    First

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+/*
+    A program to do a Linear Program (LP) to find the optimal driver and car for a Grand Prix season
+    up to that point in time.
+
+    2022-06-27    Sven Ponelat
+*/
+
+
+
+
+fn main() {
+    println!("Hello, world!");
+}
